9cdeb26c6ba1c43382f3e8a1f2944a1e0f805340
Dustin Rouillard <code@dstn.to>
2021-09-02 01:20:58 -0600

    Removing the trailing slash, not here

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,6 +2,6 @@ import { GMAuth } from "../config";
 import { Post } from "../types/Post";
 
 export async function getPosts(): Promise<Post[]> {
-  const data: { feed: Post[] } = await fetch('https://api.gm.town/posts/feed/', { headers: { authorization: `Bearer ${GMAuth}` } }).then(r => r.json());
+  const data: { feed: Post[] } = await fetch('https://api.gm.town/posts/feed', { headers: { authorization: `Bearer ${GMAuth}` } }).then(r => r.json());
   return data.feed.reverse();
 }
