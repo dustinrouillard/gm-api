55d5825196f290bf34386902e4fc7b5f7adffa2c
Dustin Rouillard <code@dstn.to>
2021-09-03 22:18:58 -0600

    Removing uneeded routes

--- src/server.ts
+++ src/server.ts
@@ -7,11 +7,10 @@ import fastifyCors from 'fastify-cors';
 import { Log, SetConfig } from '@dustinrouillard/fastify-utilities/modules/logger';
 SetConfig({ disableTimestamp: true, logColor: 'blueBright' });
 
-import { RedisClient } from '@dustinrouillard/database-connectors/redis';
-import { Success, Failed } from '@dustinrouillard/fastify-utilities/modules/response';
+import { Success } from '@dustinrouillard/fastify-utilities/modules/response';
 import { Logger, Missing } from '@dustinrouillard/fastify-utilities/modules/request';
 import { PostgresClient } from '@dustinrouillard/database-connectors/postgres';
-import { getLb, postGm } from './utils/api';
+import { getLb } from './utils/api';
 
 const server = fastify();
 
@@ -21,27 +20,6 @@ server.register(fastifyCors);
 server.register(Logger());
 
 // Routes
-server.get('/username/:username', async (req: FastifyRequest<{ Params: { username: string } }>, reply) => {
-  const user = await PostgresClient.oneOrNone('SELECT users.id, score, username, name, bio, avatar, rank FROM users LEFT JOIN ranks ON users.id = ranks.id WHERE username = $1 ORDER BY score DESC LIMIT 1;', [
-    req.params.username
-  ]);
-  if (!user) return Failed(reply, 404, 'user_not_found');
-
-  const lastPost = await PostgresClient.oneOrNone('SELECT id, creation_time, type, creator FROM posts WHERE creator = $1 ORDER BY creation_time DESC LIMIT 1;', [user.id]);
-
-  return Success(reply, 200, { user, last_post: lastPost });
-});
-
-server.get('/search', async (req: FastifyRequest<{ Querystring: { query: string } }>, reply) => {
-  const results = await PostgresClient.manyOrNone(
-    "SELECT users.id, score, username, name, bio, avatar, rank FROM users LEFT JOIN ranks ON users.id = ranks.id WHERE username = $1 OR username LIKE CONCAT($1, '%') ORDER BY score DESC LIMIT 25;",
-    [req.query.query]
-  );
-  if (!results) return Failed(reply, 404, 'query_not_found');
-
-  return Success(reply, 200, { results });
-});
-
 server.get('/top', async (req: FastifyRequest, reply) => {
   const users = await PostgresClient.manyOrNone('SELECT users.id, score, username, name, bio, avatar, rank FROM users LEFT JOIN ranks ON users.id = ranks.id WHERE users.hidden IS FALSE ORDER BY rank ASC LIMIT 100;');
 
@@ -54,24 +32,6 @@ server.get('/top/official', async (req: FastifyRequest, reply) => {
   return Success(reply, 200, lb);
 });
 
-server.get('/gm/last', async (req: FastifyRequest, reply) => {
-  const last = await RedisClient.get('last/gm');
-
-  return Success(reply, 200, last);
-});
-
-server.post('/gm', async (req: FastifyRequest, reply) => {
-  if (await RedisClient.exists('last/gm')) return Failed(reply, 400, 'already_said_gm_today');
-
-  await postGm();
-
-  const tomorrows_date = new Date();
-  tomorrows_date.setHours(24, 0, 0, 0);
-
-  await RedisClient.set('last/gm', new Date().toISOString(), 'pxat', tomorrows_date.getTime());
-
-  return Success(reply, 201);
-});
 
 server.get('/recents', async (req: FastifyRequest, reply) => {
   const posts = await PostgresClient.manyOrNone(`
